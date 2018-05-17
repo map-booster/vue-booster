6883577a6522b7d99da5496aa7438fc263dcff2d
Ryan DeBeasi <rdebeasi@redhat.com>
2018-05-17 17:29:03 -0400

    Update readme and fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+      "eslint:recommended",
+      "plugin:vue/essential"
+    ],
     "parserOptions": {
         "sourceType": "module"
     },
