ec97c93da988d42d4bfeef5466d88518de65e5e7
Ryan DeBeasi <rdebeasi@redhat.com>
2018-05-21 15:37:03 -0400

    Add semantic UI and begin building sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
-<template>
-  <div id="app">
-    <Map />
-  </div>
+  <template>
+  <sui-sidebar-pushable>
+    <!-- <side-menu v-bind:visible="sidebarVisible" /> -->
+    <sui-button v-on:click="toggleSidebar" class="sidebarToggle simpleBtn">
+      <sui-icon name="sidebar" size="big" />
+    </sui-button>
+    <sui-sidebar-pusher className="contentArea">
+      <map-view />
+    </sui-sidebar-pusher>
+  </sui-sidebar-pushable>  
 </template>
 
 <script>
-import Map from './components/Map.vue';
+import MapView from './components/MapView.vue';
+// import SideMenu from './components/SideMenu.vue';
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      sidebarVisible: true
+    };
+  },
   components: {
-    Map
+    MapView
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarVisible = !this.sidebarVisible;
+    }
   }
 };
 </script>
 
 <style>
+.sidebarToggle {
+  position: absolute;
+  left: 0;
+  z-index: 100;
+}
 
+.ui.button.simpleBtn {
+  padding: 10px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+.contentArea {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>sideMenu</h1>
+</template>
+
+<script>
+export default {
+  name: 'sideMenu'
+};
+</script>
+
+<style>
+</style>
